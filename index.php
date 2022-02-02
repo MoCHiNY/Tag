54a476e19d42eae7ea829148f3b272532ca39375
Yura <terebill2@gmail.com>
2022-02-02 17:48:05 +0200

    Визов обєкта через душки

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ foreach ($arr as $value) {
   require_once($value . ".php");
 }
 
-$arr = ['id' => 'test', 'class' => 'eee bbb'];
+// $arr = ['id' => 'test', 'class' => 'eee bbb'];
 
-$tag = new Tag('input');
-$tag->setAttr('disable', true);
-echo $tag->open(); // выведет <input id="test" class="eee bbb">
+// $tag = new Tag('input');
+// $tag->setAttr('disable', true);
+// echo $tag->open(); // выведет <input id="test" class="eee bbb">
+
+echo (new Tag('input'))->setAttr('name', 'name1')->open();
+echo (new Tag('input'))->setAttr('name1', 'name2')->open();
